a4163b920ba5e9f76e561b5bf2723e59ce9d276f
kevins981 <kevinsong981@gmail.com>
2022-10-22 00:09:12 -0400

    -m option not compatibe with -w

--- gen_graphs.sh
+++ gen_graphs.sh
@@ -7,9 +7,9 @@
 #./converter -u30 -k16 -m -b benchmark/graphs/urand_30.sg
 
 ### weighted for SSSP
-./converter -u28 -k16 -m -wb benchmark/graphs/urand_28.wsg
-#./converter -u29 -k16 -m -wb benchmark/graphs/urand_29.wsg
-#./converter -u30 -k16 -m -wb benchmark/graphs/urand_30.wsg
+./converter -u28 -k16 -wb benchmark/graphs/urand_28.wsg
+#./converter -u29 -k16 -wb benchmark/graphs/urand_29.wsg
+#./converter -u30 -k16 -wb benchmark/graphs/urand_30.wsg
 
 ##### kron graphs
 #./converter -g28 -k16 -m -b benchmark/graphs/kron_28.sg
@@ -17,7 +17,7 @@
 #./converter -g30 -k16 -m -b benchmark/graphs/kron_30.sg
 
 ### weighted for SSSP
-./converter -g28 -k16 -m -wb benchmark/graphs/kron_28.wsg
-#./converter -g29 -k16 -m -wb benchmark/graphs/kron_29.wsg
-#./converter -g30 -k16 -m -wb benchmark/graphs/kron_30.wsg
+./converter -g28 -k16 -wb benchmark/graphs/kron_28.wsg
+#./converter -g29 -k16 -wb benchmark/graphs/kron_29.wsg
+#./converter -g30 -k16 -wb benchmark/graphs/kron_30.wsg
 
