5327059ccb2a33e6805a6314e75bf5e9cbef9970
kevins981 <kevinsong981@gmail.com>
2022-11-10 15:59:26 -0500

    Add scripts.

---
+++ run_exp_cxl_bw.sh
@@ -0,0 +1,211 @@
+#!/bin/bash
+
+# Run experiments that place all data structures on node 0 DRAM vs node 1 DRAM.
+
+GRAPH_DIR="/ssd1/songxin8/thesis/graph/gapbs/benchmark/graphs"
+NUM_THREADS=16
+export OMP_NUM_THREADS=${NUM_THREADS}
+RESULT_DIR="exp/exp_cxl_bw_5_19"
+
+declare -a GRAPH_LIST=("kron_28")
+#declare -a EXE_LIST=("bfs" "cc" "bc") 
+declare -a EXE_LIST=("bfs" "cc")
+
+clean_up () {
+    echo "Cleaning up. Kernel PID is $EXE_PID, numastat PID is $LOG_PID."
+    # Perform program exit housekeeping 
+    kill $LOG_PID
+    kill $EXE_PID
+    exit
+}
+
+
+clean_cache () { 
+  echo "Clearing caches..."
+  # clean CPU caches
+  ./tools/clear_cpu_cache
+  # clean page cache
+  echo 3 > /proc/sys/vm/drop_caches
+}
+
+run_gap () { 
+  OUTFILE=$1 #first argument
+  GRAPH=$2
+  EXE=$3
+  echo "Start" > $OUTFILE
+  #numastat -v &>> $OUTFILE
+  
+  case $EXE in
+    "bfs")
+      /usr/bin/time -v /usr/bin/numactl --membind=0 --cpunodebind=0 \
+          ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -n200 &>> $OUTFILE &
+      TIME_PID=$! 
+      EXE_PID=$(pgrep -P $TIME_PID)
+      ;;
+    "pr")
+      /usr/bin/time -v /usr/bin/numactl --membind=0 --cpunodebind=0 \
+          ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -i1000 -t1e-4 -n10 &>> $OUTFILE &
+      TIME_PID=$! 
+      EXE_PID=$(pgrep -P $TIME_PID)
+      ;;
+    "cc")
+      /usr/bin/time -v /usr/bin/numactl --membind=0 --cpunodebind=0 \
+          ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -n200 &>> $OUTFILE &
+      TIME_PID=$! 
+      EXE_PID=$(pgrep -P $TIME_PID)
+      ;;
+    "bc")
+      /usr/bin/time -v /usr/bin/numactl --membind=0 --cpunodebind=0 \
+          ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -i4 -n8 &>> $OUTFILE &
+      # PID of time command
+      TIME_PID=$! 
+      # get PID of actual GAP kernel, which is a child of time. 
+      # This PID is needed for the numastat command
+      EXE_PID=$(pgrep -P $TIME_PID)
+      ;; 
+    #"sssp")
+    #  /usr/bin/time -v /usr/bin/numactl --membind=0 --cpunodebind=0 \
+    #      ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.wsg -d2 -n1 &>> $OUTFILE &
+    #  TIME_PID=$! 
+    #  EXE_PID=$(pgrep -P $TIME_PID)
+    #  ;;
+    #"tc")
+    #  /usr/bin/time -v /usr/bin/numactl --membind=0 --cpunodebind=0 \
+    #      ./${EXE} -f ${GRAPH_DIR}/${GRAPH}U.sg -n1 &>> $OUTFILE &
+    #  TIME_PID=$! 
+    #  EXE_PID=$(pgrep -P $TIME_PID)
+    #  ;;
+    *)
+      echo -n "Unknown executable $EXE"
+      ;;
+  esac
+
+  echo "EXE PID is ${EXE_PID}"
+  echo "start" > ${OUTFILE}_numastat
+  while true; do numastat -p $EXE_PID >> ${OUTFILE}_numastat; sleep 2; done &
+  LOG_PID=$!
+
+  echo "Waiting for GAP kernel to complete (PID is ${EXE_PID}). numastat is logged into ${OUTFILE}_numastat, PID is ${LOG_PID}" 
+  wait $TIME_PID
+  echo "GAP kernel complete."
+  kill $LOG_PID
+}
+
+
+run_gap_autonuma () { 
+  OUTFILE=$1 #first argument
+  GRAPH=$2
+  EXE=$3
+
+
+  echo "Start" > $OUTFILE
+  #numastat -v &>> $OUTFILE
+  
+  case $EXE in
+    "pr")
+      # not setting --membind=0. Let AutoNUMA decide where to allocate memory.
+      # however the processors have to stay on node 0.
+      /usr/bin/time -v /usr/bin/numactl --cpunodebind=0 \
+          ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -i1000 -t1e-4 -n10 &>> $OUTFILE &
+      TIME_PID=$! 
+      EXE_PID=$(pgrep -P $TIME_PID)
+      ;;
+    "bfs")
+      /usr/bin/time -v /usr/bin/numactl --cpunodebind=0 \
+          ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -n100 &>> $OUTFILE &
+      TIME_PID=$! 
+      EXE_PID=$(pgrep -P $TIME_PID)
+      ;;
+    "pr")
+      /usr/bin/time -v /usr/bin/numactl --cpunodebind=0 \
+          ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -i1000 -t1e-4 -n10 &>> $OUTFILE &
+      TIME_PID=$! 
+      EXE_PID=$(pgrep -P $TIME_PID)
+      ;;
+    "cc")
+      /usr/bin/time -v /usr/bin/numactl --cpunodebind=0 \
+          ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -n100 &>> $OUTFILE &
+      TIME_PID=$! 
+      EXE_PID=$(pgrep -P $TIME_PID)
+      ;;
+    "bc")
+      /usr/bin/time -v /usr/bin/numactl --cpunodebind=0 \
+          ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -i4 -n8 &>> $OUTFILE &
+      # PID of time command
+      TIME_PID=$! 
+      # get PID of actual GAP kernel, which is a child of time. 
+      # This PID is needed for the numastat command
+      EXE_PID=$(pgrep -P $TIME_PID)
+      ;; 
+    *)
+      echo -n "Unknown executable $EXE"
+      ;;
+  esac
+
+  echo "EXE PID is ${EXE_PID}"
+  echo "start" > ${OUTFILE}_numastat
+  while true; do numastat -p $EXE_PID >> ${OUTFILE}_numastat; sleep 2; done &
+  LOG_PID=$!
+
+  echo "Waiting for GAP kernel to complete (PID is ${EXE_PID}). numastat is logged into ${OUTFILE}_numastat, PID is ${LOG_PID}" 
+  wait $TIME_PID
+  echo "GAP kernel complete."
+  kill $LOG_PID
+}
+
+
+##############
+# Script start
+##############
+trap clean_up SIGHUP SIGINT SIGTERM
+
+[[ $EUID -ne 0 ]] && echo "This script must be run using sudo or as root." && exit 1
+
+./setup.sh
+
+mkdir -p $RESULT_DIR
+
+
+# not specifying where to allocate. Let AutoNUMA decide 
+make clean -j
+make -j
+
+sudo service numad start
+NUMAD_OUT=$(systemctl is-active numad)
+echo "numad service is now $NUMAD_OUT (should be active)"
+
+echo 1 > /proc/sys/kernel/numa_balancing
+NUMA_BALANCING=$(cat /proc/sys/kernel/numa_balancing)
+echo "numa_balancing is now $NUMA_BALANCING (should be 1)"
+
+for graph in "${GRAPH_LIST[@]}"
+do
+  for exe in "${EXE_LIST[@]}"
+  do
+    clean_cache
+    run_gap_autonuma "${RESULT_DIR}/${exe}_${graph}_${NUM_THREADS}threads_autonuma2" $graph $exe
+  done
+done
+
+
+# allocate neighbors array on node 1
+# turn off both numa
+sudo service numad stop
+NUMAD_OUT=$(systemctl is-active numad)
+echo "numad service is now $NUMAD_OUT (should be not active)"
+
+echo 0 > /proc/sys/kernel/numa_balancing
+NUMA_BALANCING=$(cat /proc/sys/kernel/numa_balancing)
+echo "numa_balancing is now $NUMA_BALANCING (should be 0)"
+
+make clean -j
+make neigh_on_numa1 -j
+
+for graph in "${GRAPH_LIST[@]}"
+do
+  for exe in "${EXE_LIST[@]}"
+  do
+    clean_cache
+    run_gap "${RESULT_DIR}/${exe}_${graph}_${NUM_THREADS}threads_neigh_on_numa1" $graph $exe
+  done
+done
---
+++ run_vtune_cxl_bw.sh
@@ -0,0 +1,279 @@
+#!/bin/bash
+
+GRAPH_DIR="/ssd1/songxin8/thesis/graph/gapbs/benchmark/graphs"
+RESULT_DIR="/ssd1/songxin8/thesis/graph/vtune/exp_cxl_bw/"
+
+NUM_THREADS=16
+export OMP_NUM_THREADS=${NUM_THREADS}
+
+#declare -a GRAPH_LIST=("kron_28" "urand_28")
+declare -a GRAPH_LIST=("kron_28")
+#declare -a EXE_LIST=("cc" "bc"  "sssp" "bfs" "tc")
+declare -a EXE_LIST=("bfs")
+
+export LD_PRELOAD="/usr/lib/x86_64-linux-gnu/debug/libstdc++.so.6.0.28"
+echo "LD_PRELOAD: $LD_PRELOAD"
+
+clean_cache () { 
+  echo "Clearing caches..."
+  # clean CPU caches
+  ./tools/clear_cpu_cache
+  # clean page cache
+  echo 3 > /proc/sys/vm/drop_caches
+}
+
+run_vtune () {
+  OUTFILE=$1 #first argument
+  GRAPH=$2
+  EXE=$3
+
+  VTUNE_MEMACC_COMMON="/opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -collect memory-access -start-paused \
+      -knob sampling-interval=10 -knob analyze-mem-objects=true -knob analyze-openmp=true \
+      -data-limit=10000 -result-dir ${RESULT_DIR}/${OUTFILE}_memacc \
+      --app-working-dir=/ssd1/songxin8/thesis/graph/gapbs"
+
+  #VTUNE_HOTSPOT_COMMON="/opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -collect hotspots -start-paused \
+  #    -data-limit=10000 -result-dir ${RESULT_DIR}/${OUTFILE}_hotspot \
+  #    --app-working-dir=/ssd1/songxin8/thesis/graph/gapbs"
+
+  VTUNE_UARCH_COMMON="/opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -collect uarch-exploration -start-paused \
+      -knob sampling-interval=10 -knob collect-memory-bandwidth=true
+      -data-limit=10000 -result-dir ${RESULT_DIR}/${OUTFILE}_uarch \
+      --app-working-dir=/ssd1/songxin8/thesis/graph/gapbs"
+
+  NUMACTL_COMMOM="/usr/bin/numactl --membind=0 --cpunodebind=0"
+
+  case $EXE in
+    "bfs")
+      #${VTUNE_HOTSPOT_COMMON} -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -n1000000 &
+      #clean_cache
+
+      ${VTUNE_MEMACC_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -n1000000 &
+      sleep 600
+      /opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -command stop -r ${RESULT_DIR}/${OUTFILE}_memacc
+      clean_cache
+
+      #${VTUNE_UARCH_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -n1000000 &
+      ;;
+    "pr")
+      #${VTUNE_HOTSPOT_COMMON} -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -i1000 -t1e-4 -n800000  &
+      #clean_cache
+
+      ${VTUNE_MEMACC_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -i1000 -t1e-4 -n800000  &
+      sleep 600
+      /opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -command stop -r ${RESULT_DIR}/${OUTFILE}_memacc
+      clean_cache
+
+      #${VTUNE_UARCH_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -i1000 -t1e-4 -n800000  &
+      ;;
+    "cc")
+      #${VTUNE_HOTSPOT_COMMON} -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -n18800000  &
+      #clean_cache
+
+      ${VTUNE_MEMACC_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -n18800000  &
+      sleep 600
+      /opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -command stop -r ${RESULT_DIR}/${OUTFILE}_memacc
+      clean_cache
+
+      #${VTUNE_UARCH_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -n18800000  &
+      ;;
+    "bc")
+      #${VTUNE_HOTSPOT_COMMON} -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -i4 -n400000  &
+      #clean_cache
+
+      ${VTUNE_MEMACC_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -i4 -n400000  &
+      sleep 600
+      /opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -command stop -r ${RESULT_DIR}/${OUTFILE}_memacc
+      clean_cache
+
+      #${VTUNE_UARCH_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -i4 -n400000  &
+      ;;
+    "sssp")
+      #${VTUNE_HOTSPOT_COMMON} -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.wsg -d2 -n100000  &
+      #clean_cache
+
+      ${VTUNE_MEMACC_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.wsg -d2 -n100000 &
+      sleep 600
+      /opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -command stop -r ${RESULT_DIR}/${OUTFILE}_memacc
+      clean_cache
+
+      #${VTUNE_UARCH_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.wsg -d2 -n100000 &
+      #sleep 600
+      #/opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -command stop -r ${RESULT_DIR}/${OUTFILE}_uarch
+      ;;
+    "tc")
+      #${VTUNE_HOTSPOT_COMMON} -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}U.sg -n1000000 &
+      #clean_cache
+
+      ${VTUNE_MEMACC_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}U.sg -n100000 &
+      sleep 600
+      /opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -command stop -r ${RESULT_DIR}/${OUTFILE}_memacc
+      clean_cache
+
+      #${VTUNE_UARCH_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}U.sg -n100000 &
+      #sleep 600
+      #/opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -command stop -r ${RESULT_DIR}/${OUTFILE}_uarch
+      ;;
+
+    *)
+      echo -n "Unknown executable $EXE"
+      ;;
+  esac
+
+}
+
+run_vtune_autonuma () {
+  OUTFILE=$1 #first argument
+  GRAPH=$2
+  EXE=$3
+
+
+  VTUNE_MEMACC_COMMON="/opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -collect memory-access -start-paused \
+      -knob sampling-interval=10 -knob analyze-mem-objects=true -knob analyze-openmp=true \
+      -data-limit=10000 -result-dir ${RESULT_DIR}/${OUTFILE}_memacc \
+      --app-working-dir=/ssd1/songxin8/thesis/graph/gapbs"
+
+  #VTUNE_HOTSPOT_COMMON="/opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -collect hotspots -start-paused \
+  #    -data-limit=10000 -result-dir ${RESULT_DIR}/${OUTFILE}_hotspot \
+  #    --app-working-dir=/ssd1/songxin8/thesis/graph/gapbs"
+
+  VTUNE_UARCH_COMMON="/opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -collect uarch-exploration -start-paused \
+      -knob sampling-interval=10 -knob collect-memory-bandwidth=true
+      -data-limit=10000 -result-dir ${RESULT_DIR}/${OUTFILE}_uarch \
+      --app-working-dir=/ssd1/songxin8/thesis/graph/gapbs"
+
+  # do not bind memory to node 0. Let AutoNUMA decide
+  NUMACTL_COMMOM="/usr/bin/numactl --cpunodebind=0"
+
+  case $EXE in
+    "bfs")
+      #${VTUNE_HOTSPOT_COMMON} -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -n1000000 &
+      #clean_cache
+
+      ${VTUNE_MEMACC_COMMON} -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -n1000000 &
+      echo "command is" 
+      echo "${VTUNE_MEMACC_COMMON} -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -n1000000 &"
+
+      sleep 600
+      /opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -command stop -r ${RESULT_DIR}/${OUTFILE}_memacc
+      clean_cache
+
+      #${VTUNE_UARCH_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -n1000000 &
+      ;;
+    "pr")
+      #${VTUNE_HOTSPOT_COMMON} -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -i1000 -t1e-4 -n800000  &
+      #clean_cache
+
+      ${VTUNE_MEMACC_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -i1000 -t1e-4 -n800000  &
+      sleep 600
+      /opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -command stop -r ${RESULT_DIR}/${OUTFILE}_memacc
+      clean_cache
+
+      #${VTUNE_UARCH_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -i1000 -t1e-4 -n800000  &
+      ;;
+    "cc")
+      #${VTUNE_HOTSPOT_COMMON} -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -n18800000  &
+      #clean_cache
+
+      ${VTUNE_MEMACC_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -n18800000  &
+      sleep 600
+      /opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -command stop -r ${RESULT_DIR}/${OUTFILE}_memacc
+      clean_cache
+
+      #${VTUNE_UARCH_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -n18800000  &
+      ;;
+    "bc")
+      #${VTUNE_HOTSPOT_COMMON} -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -i4 -n400000  &
+      #clean_cache
+
+      ${VTUNE_MEMACC_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -i4 -n400000  &
+      sleep 600
+      /opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -command stop -r ${RESULT_DIR}/${OUTFILE}_memacc
+      clean_cache
+
+      #${VTUNE_UARCH_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.sg -i4 -n400000  &
+      ;;
+    "sssp")
+      #${VTUNE_HOTSPOT_COMMON} -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.wsg -d2 -n100000  &
+      #clean_cache
+
+      ${VTUNE_MEMACC_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.wsg -d2 -n100000 &
+      sleep 600
+      /opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -command stop -r ${RESULT_DIR}/${OUTFILE}_memacc
+      clean_cache
+
+      #${VTUNE_UARCH_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}.wsg -d2 -n100000 &
+      #sleep 600
+      #/opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -command stop -r ${RESULT_DIR}/${OUTFILE}_uarch
+      ;;
+    "tc")
+      #${VTUNE_HOTSPOT_COMMON} -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}U.sg -n1000000 &
+      #clean_cache
+
+      ${VTUNE_MEMACC_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}U.sg -n100000 &
+      sleep 600
+      /opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -command stop -r ${RESULT_DIR}/${OUTFILE}_memacc
+      clean_cache
+
+      #${VTUNE_UARCH_COMMON}  -- ${NUMACTL_COMMOM} ./${EXE} -f ${GRAPH_DIR}/${GRAPH}U.sg -n100000 &
+      #sleep 600
+      #/opt/intel/oneapi/vtune/2022.3.0/bin64/vtune -command stop -r ${RESULT_DIR}/${OUTFILE}_uarch
+      ;;
+
+    *)
+      echo -n "Unknown executable $EXE"
+      ;;
+  esac
+
+}
+
+##############
+# Script start
+##############
+
+[[ $EUID -ne 0 ]] && echo "This script must be run using sudo or as root." && exit 1
+
+./setup.sh
+
+make clean
+make -j
+#service numad should be active
+sudo service numad start
+NUMAD_OUT=$(systemctl is-active numad)
+echo "numad service is now $NUMAD_OUT (should be active)"
+
+echo 1 > /proc/sys/kernel/numa_balancing
+NUMA_BALANCING=$(cat /proc/sys/kernel/numa_balancing)
+echo "numa_balancing is now $NUMA_BALANCING (should be 1)"
+
+echo "Number of threads: ${OMP_NUM_THREADS}" 
+
+for graph in "${GRAPH_LIST[@]}"
+do
+  for exe in "${EXE_LIST[@]}"
+  do
+    clean_cache
+    run_vtune_autonuma "${exe}_${graph}_${NUM_THREADS}threads_autonuma" $graph $exe
+  done
+done
+
+make clean
+make neigh_on_numa1 -j
+# turn off both numa
+sudo service numad stop
+NUMAD_OUT=$(systemctl is-active numad)
+echo "numad service is now $NUMAD_OUT (should be not active)"
+
+echo 0 > /proc/sys/kernel/numa_balancing
+NUMA_BALANCING=$(cat /proc/sys/kernel/numa_balancing)
+echo "numa_balancing is now $NUMA_BALANCING (should be 0)"
+
+for graph in "${GRAPH_LIST[@]}"
+do
+  for exe in "${EXE_LIST[@]}"
+  do
+    clean_cache
+    run_vtune "${exe}_${graph}_${NUM_THREADS}threads_neigh_on_numa1" $graph $exe
+  done
+done
+
