5d583d1137ca7f13cf415e3f69d25aab7f551c8d
kevins981 <kevinsong981@gmail.com>
2022-11-28 12:13:56 -0500

    Remove unused files

--- setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-[[ $EUID -ne 0 ]] && echo "This script must be run using sudo or as root." && exit 1
-
-echo "Running setup script. Turning off Linux NUMA."
-
-# Do not allow Linux NUMA to move data around
-service numad stop
-echo 0 > /proc/sys/kernel/numa_balancing
